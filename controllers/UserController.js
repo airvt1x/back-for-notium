91f40ecae8acdb187acf4803276313f64c0278d0
airvt1x <airat3552@gmail.com>
2024-04-23 19:37:51 +0300

    not first update

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -80,3 +80,24 @@ export const login = async(req, res)=>{
         });
     }
 };
+
+export const getMe = async (req,res) => {
+    try {
+        const user = await UserModel.findById(req.userId);
+
+        if(!user){
+            return res.status(404).json({
+                message:'Пользователь не найден'
+            });
+        }
+
+        const {passwordHash, ...userData} = user._doc;
+
+        res.json(userData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Нет доступа',
+        });
+    }
+};
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 
-import handleValidationErrors from "./utils/handleValidationErrors.js";
+
+import {checkAuth, handleValidationErrors} from "./utils/index.js";
 import { UserController } from "./controllers/index.js";
 
 mongoose.set("strictQuery", false);
 
 mongoose
-    .connect('mongodb+srv://airvt1x:airvt1xairvt1x@clasteric.adybi34.mongodb.net/?retryWrites=true&w=majority&appName=clasteric')
+    .connect('mongodb+srv://airvt1x:airvt1xairvt1x@clasteric.adybi34.mongodb.net/notium?retryWrites=true&w=majority&appName=clasteric')
     .then(() => console.log('DB ok'))
     .catch((err) => console.log('DB error', err));
 
@@ -21,5 +22,9 @@ app.listen(1234, (err)=>{
     console.log('Server OK ');
 });
 
+app.get('/', (req,res) => {
+    res.send('Гугуца батрачит')
+});
 app.post('/auth/login', handleValidationErrors, UserController.login);
 app.post('/auth/register', handleValidationErrors, UserController.register);
+app.get('/auth/me', checkAuth, UserController.getMe);
