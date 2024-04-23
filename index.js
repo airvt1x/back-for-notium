2159c0c1b8a6ef549e387b5073307e3493f0a00a
airvt1x <airat3552@gmail.com>
2024-04-23 22:39:19 +0300

    added cocks128

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(cors({
 
 const allowCors = fn => async (req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Origin', 'https://notium-app.vercel.app/')
     // another common pattern
     // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
