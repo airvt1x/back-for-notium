5bbf5d1c6325cd916ce747da1adf35cb4feef75d
airvt1x <airat3552@gmail.com>
2024-05-02 22:57:17 +0300

    notes

--- controllers/NoteController.js
+++ controllers/NoteController.js
@@ -3,9 +3,7 @@ import NoteModel from "../models/Note.js"
 export const getNotes = async(req,res)=>{
     const user_id = req.userId
     try {
-        const notes = await NoteModel.find({
-            user: user_id
-        });
+        const notes = await NoteModel.find({user: user_id});
 
         res.json(notes);
     } catch (err) {
